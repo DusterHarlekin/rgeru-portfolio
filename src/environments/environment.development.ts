048946647f989d0286a5d5dd7da98c724bfd327a
Roger Guedez <50921749+DusterHarlekin@users.noreply.github.com>
2023-04-30 16:37:24 -0400

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,3 @@
 export const environment = {
-    URL: 'https://fvbjeqgwqeopdcpxttec.supabase.co',
-    KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2YmplcWd3cWVvcGRjcHh0dGVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA5MTMwNzEsImV4cCI6MTk5NjQ4OTA3MX0.LlbBkrVBRhIslHw_YOZfLpduKH8N-d7TdWjXRx4No0o'
+    
 };
