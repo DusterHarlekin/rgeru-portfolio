3fabf98e0aef04bebe58718acd5880359778f444
Roger Guedez <guedez66@gmail.com>
2023-04-30 16:29:22 -0400

    lang and database interaction, login and control panel with individual image CRUD and category CRUD service

--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -1,99 +1,90 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { createClient } from '@supabase/supabase-js'
-import { environment } from 'src/environments/environment'
+import { createClient } from '@supabase/supabase-js';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SupabaseService {
-  private supabase: any
-  private session:any
-  constructor(private router:Router) {
-    this.supabase = createClient(environment.URL, environment.KEY)
+  private supabase: any;
+  private session: any;
+  constructor(private router: Router) {
+    this.supabase = createClient(environment.URL, environment.KEY);
   }
 
-  signIn(email:string, password:string){
+  signIn(email: string, password: string) {
     return this.supabase.auth.signInWithPassword({
       email: email,
       password: password,
-    })
+    });
   }
 
-  deleteCategory(categoryId: any){
+  deleteCategory(categoryId: any) {
+    return this.supabase.from('categories').delete().eq('id', categoryId);
+  }
+
+  addCategory(categoryName: any) {
     return this.supabase
-    .from('categories')
-    .delete()
-    .eq('id', categoryId)
+      .from('categories')
+      .insert({ name: categoryName })
+      .select();
   }
 
-  addCategory(categoryName: any){
-    return this.supabase
-    .from('categories')
-    .insert({ name: categoryName })
-    .select()
-  }
-
- getCategories(){
-    /*
-    const result = await this.supabase
-      .from('tasks')
-      .select()
-      .eq('userId', user?.id)
-      .eq('done', done)
-      .order('id', { ascending: true });
-    console.log(result);
-    console.log(result.data);
-    */
-
+  getCategories() {
     return this.supabase
       .from('categories')
       .select()
       .order('id', { ascending: true });
   }
 
-  getAllImg(){
+  getAllImg() {
     return this.supabase
-    .from('images')
-    .select()
-    .order('id', { ascending: false });
+      .from('images')
+      .select()
+      .order('id', { ascending: false });
   }
 
-  addImg(addImgForm: any){
+  addImg(addImgForm: any) {
     return this.supabase
-    .from('images')
-    .insert({ url: addImgForm.value.url, category_id: addImgForm.value.category_id, isLatest: addImgForm.value.isLatest, isMature: addImgForm.value.isMature})
-    .select()
+      .from('images')
+      .insert({
+        url: addImgForm.value.url,
+        category_id: addImgForm.value.category_id,
+        isLatest: addImgForm.value.isLatest,
+        isMature: addImgForm.value.isMature
+      })
+      .select();
   }
 
-  updateImg(updateImgForm: any){
+  updateImg(updateImgForm: any) {
     return this.supabase
-    .from('images')
-    .update({ url: updateImgForm.value.url, category_id: updateImgForm.value.category_id, isLatest: updateImgForm.value.isLatest, isMature: updateImgForm.value.isMature})
-    .eq('id', updateImgForm.value.id)
-    .select()
+      .from('images')
+      .update({
+        url: updateImgForm.value.url,
+        category_id: updateImgForm.value.category_id,
+        isLatest: updateImgForm.value.isLatest,
+        isMature: updateImgForm.value.isMature,
+      })
+      .eq('id', updateImgForm.value.id)
+      .select();
   }
 
-  deleteImg(imgId:number){
-    
-    return this.supabase
-    .from('images')
-    .delete()
-    .eq('id', imgId)
-
+  deleteImg(imgId: number) {
+    return this.supabase.from('images').delete().eq('id', imgId);
   }
 
-  async signOut(){
-    const { error } = await this.supabase.auth.signOut()
+  async signOut() {
+    const { error } = await this.supabase.auth.signOut();
     this.router.navigate(['/login']);
   }
 
-  getSession(){
+  getSession() {
     return this.session;
   }
 
-  watch(){
-    this.supabase.auth.onAuthStateChange((event:any, session:any) => {
+  watch() {
+    this.supabase.auth.onAuthStateChange((event: any, session: any) => {
       //CONTROL DE PAGE PARA LOGIN
       if (!session) {
         if (this.router.url.includes('/panel'))
